3369a72d9cc8c63e416f645a5547f845d0be9679
Duri Chitayat <duri@durichitayat.net>
2024-03-16 15:15:24 -0500

    Add users and login page mockups

---
+++ app/users/page.tsx
@@ -0,0 +1,219 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+
+export default function Users() {
+  return (
+    <>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+
+          <div className="flex flex-col place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] items-center">
+          </div>
+                  
+              <h1 className="text-4xl">
+                Users
+              </h1>
+
+
+
+              <div className="flex flex-col overflow-x-auto shadow-md sm:rounded-lg">
+                  <div className="flex items-center  flex-column flex-wrap md:flex-row space-y-4 md:space-y-0 pb-4 bg-white dark:bg-gray-900 p-4 justify-end">
+                      <div>
+                          <button id="dropdownActionButton" data-dropdown-toggle="dropdownAction" className="inline-flex items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" type="button">
+                              <span className="sr-only">Action button</span>
+                              Action
+                              <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4"/>
+                              </svg>
+                          </button>
+                        
+                          <div id="dropdownAction" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                              <ul className="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownActionButton">
+                                  <li>
+                                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Reward</a>
+                                  </li>
+                                  <li>
+                                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Promote</a>
+                                  </li>
+                                  <li>
+                                      <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Activate account</a>
+                                  </li>
+                              </ul>
+                              <div className="py-1">
+                                  <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete User</a>
+                              </div>
+                          </div>
+                      </div>
+
+                  </div>
+                  <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                          <tr>
+                              <th scope="col" className="p-4">
+                                  <div className="flex items-center">
+                                      <input id="checkbox-all-search" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                      <label htmlFor="checkbox-all-search" className="sr-only">checkbox</label>
+                                  </div>
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Name
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Role
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Status
+                              </th>
+                              <th scope="col" className="px-6 py-3">
+                                  Action
+                              </th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                              <td className="w-4 p-4">
+                                  <div className="flex items-center">
+                                      <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                      <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
+                                  </div>
+                              </td>
+                              <th scope="row" className="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                  <img className="w-10 h-10 rounded-full" src="placeholder.png" alt="Jese image" />
+                                  <div className="ps-3">
+                                      <div className="text-base font-semibold">Neil Sims</div>
+                                      <div className="font-normal text-gray-500">neil.sims@email.com</div>
+                                  </div>  
+                              </th>
+                              <td className="px-6 py-4">
+                                  User
+                              </td>
+                              <td className="px-6 py-4">
+                                  <div className="flex items-center">
+                                      <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
+                                  </div>
+                              </td>
+                              <td className="px-6 py-4">
+                                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Block user</a>
+                              </td>
+                          </tr>
+                          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                              <td className="w-4 p-4">
+                                  <div className="flex items-center">
+                                      <input id="checkbox-table-search-2" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                      <label htmlFor="checkbox-table-search-2" className="sr-only">checkbox</label>
+                                  </div>
+                              </td>
+                              <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  <img className="w-10 h-10 rounded-full" src="placeholder.png" alt="Jese image" />
+                                  <div className="ps-3">
+                                      <div className="text-base font-semibold">Bonnie Green</div>
+                                      <div className="font-normal text-gray-500">bonnie@email.com</div>
+                                  </div>
+                              </th>
+                              <td className="px-6 py-4">
+                                  User
+                              </td>
+                              <td className="px-6 py-4">
+                                  <div className="flex items-center">
+                                      <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
+                                  </div>
+                              </td>
+                              <td className="px-6 py-4">
+                                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Block user</a>
+                              </td>
+                          </tr>
+                          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                              <td className="w-4 p-4">
+                                  <div className="flex items-center">
+                                      <input id="checkbox-table-search-2" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                      <label htmlFor="checkbox-table-search-2" className="sr-only">checkbox</label>
+                                  </div>
+                              </td>
+                              <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  <img className="w-10 h-10 rounded-full" src="placeholder.png" alt="Jese image" />
+                                  <div className="ps-3">
+                                      <div className="text-base font-semibold">Jese Leos</div>
+                                      <div className="font-normal text-gray-500">jese@email.com</div>
+                                  </div>
+                              </th>
+                              <td className="px-6 py-4">
+                                  User
+                              </td>
+                              <td className="px-6 py-4">
+                                  <div className="flex items-center">
+                                      <div className="h-2.5 w-2.5 rounded-full bg-red-500 me-2"></div> Blocked
+                                  </div>
+                              </td>
+                              <td className="px-6 py-4">
+                                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Unblock user</a>
+                              </td>
+                          </tr>
+                          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                              <td className="w-4 p-4">
+                                  <div className="flex items-center">
+                                      <input id="checkbox-table-search-2" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                      <label htmlFor="checkbox-table-search-2" className="sr-only">checkbox</label>
+                                  </div>
+                              </td>
+                              <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  <img className="w-10 h-10 rounded-full" src="placeholder.png" alt="Jese image" />
+                                  <div className="ps-3">
+                                      <div className="text-base font-semibold">Thomas Lean</div>
+                                      <div className="font-normal text-gray-500">thomes@email.com</div>
+                                  </div>
+                              </th>
+                              <td className="px-6 py-4">
+                                  User, Admin
+                              </td>
+                              <td className="px-6 py-4">
+                                  <div className="flex items-center">
+                                      <div className="h-2.5 w-2.5 rounded-full bg-green-500 me-2"></div> Online
+                                  </div>
+                              </td>
+                              <td className="px-6 py-4">
+                                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Block user</a>
+                              </td>
+                          </tr>
+                          <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
+                              <td className="w-4 p-4">
+                                  <div className="flex items-center">
+                                      <input id="checkbox-table-search-3" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                      <label htmlFor="checkbox-table-search-3" className="sr-only">checkbox</label>
+                                  </div>
+                              </td>
+                              <th scope="row" className="flex items-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                  <img className="w-10 h-10 rounded-full" src="placeholder.png" alt="Jese image" />
+                                  <div className="ps-3">
+                                      <div className="text-base font-semibold">Leslie Livingston</div>
+                                      <div className="font-normal text-gray-500">leslie@email.com</div>
+                                  </div>
+                              </th>
+                              <td className="px-6 py-4">
+                                  User
+                              </td>
+                              <td className="px-6 py-4">
+                                  <div className="flex items-center">
+                                      <div className="h-2.5 w-2.5 rounded-full bg-yellow-500 me-2"></div> Offline
+                                  </div>
+                              </td>
+                              <td className="px-6 py-4">
+                                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Block user</a>
+                              </td>
+                          </tr>
+                      </tbody>
+                  </table>
+              </div>
+
+
+
+
+
+
+          
+
+          <div></div>
+
+        </main>
+    </>
+  );
+}
