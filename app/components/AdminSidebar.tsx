540b8e039441fdc7ef4d23bb58ff82aa8ca215e1
michael-dobroski <michael-dobroski@outlook.com>
2024-03-17 16:15:46 -0500

    tacticle nuke dropped on the games pages. plus aesthetic changes and profile page

--- app/components/AdminSidebar.tsx
+++ app/components/AdminSidebar.tsx
@@ -15,7 +15,12 @@ function AuthButton() {
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
                 </button>
-                Signed in as: {session?.user?.name} <br />
+                <Link href="/profile" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">{session?.user?.name}</span>
+                </Link>
             </>
         );
     }
