81a0c00c849a8d904467f7376c2ac18c7f1cae49
michael-dobroski <michael-dobroski@outlook.com>
2024-03-17 16:37:51 -0500

    please deploy

--- app/components/AdminSidebar.tsx
+++ app/components/AdminSidebar.tsx
@@ -16,7 +16,7 @@ function AuthButton() {
                 <span className="flex-1 ms-3 whitespace-nowrap">Sign Out</span>
                 </button>
                 <Link href="/profile" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" id="profile"><g fill="none" fill-rule="evenodd"><g fill="#999" transform="translate(-140 -2159)"><g transform="translate(56 160)"><path d="M100.563 2017H87.438c-.706 0-1.228-.697-.961-1.338 1.236-2.964 4.14-4.662 7.523-4.662 3.384 0 6.288 1.698 7.524 4.662.267.641-.255 1.338-.961 1.338m-10.646-12c0-2.206 1.832-4 4.083-4 2.252 0 4.083 1.794 4.083 4s-1.831 4-4.083 4c-2.251 0-4.083-1.794-4.083-4m14.039 11.636c-.742-3.359-3.064-5.838-6.119-6.963 1.619-1.277 2.563-3.342 2.216-5.603-.402-2.623-2.63-4.722-5.318-5.028-3.712-.423-6.86 2.407-6.86 5.958 0 1.89.894 3.574 2.289 4.673-3.057 1.125-5.377 3.604-6.12 6.963-.27 1.221.735 2.364 2.01 2.364h15.892c1.276 0 2.28-1.143 2.01-2.364"></path></g></g></g></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" id="profile"><g fill="none" fillRule="evenodd"><g fill="#999" transform="translate(-140 -2159)"><g transform="translate(56 160)"><path d="M100.563 2017H87.438c-.706 0-1.228-.697-.961-1.338 1.236-2.964 4.14-4.662 7.523-4.662 3.384 0 6.288 1.698 7.524 4.662.267.641-.255 1.338-.961 1.338m-10.646-12c0-2.206 1.832-4 4.083-4 2.252 0 4.083 1.794 4.083 4s-1.831 4-4.083 4c-2.251 0-4.083-1.794-4.083-4m14.039 11.636c-.742-3.359-3.064-5.838-6.119-6.963 1.619-1.277 2.563-3.342 2.216-5.603-.402-2.623-2.63-4.722-5.318-5.028-3.712-.423-6.86 2.407-6.86 5.958 0 1.89.894 3.574 2.289 4.673-3.057 1.125-5.377 3.604-6.12 6.963-.27 1.221.735 2.364 2.01 2.364h15.892c1.276 0 2.28-1.143 2.01-2.364"></path></g></g></g></svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">{session?.user?.name}</span>
                 </Link>
 
@@ -43,7 +43,9 @@ export default function AdminSidebar() {
         <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
 
-                <Image src="/clueless_logo.png" alt="Clueless" width={200} height={100} className='p-4' />
+                <Link href="/">
+                    <Image src="/clueless_logo.png" alt="Clueless" width={200} height={100} className='p-4' />
+                </Link>
                 
                 <ul className="space-y-2 font-medium">
                     
