bc9456f855a8abf0745b869507c63df4533d5b14
michael-dobroski <michael-dobroski@outlook.com>
2024-03-17 11:07:34 -0500

    nextauth implemented, session is stored on some pages, roughly added to sidebar

--- app/components/AdminSidebar.tsx
+++ app/components/AdminSidebar.tsx
@@ -1,4 +1,25 @@
+"use client";
 import Link from 'next/link';
+import { signIn, signOut, useSession } from "next-auth/react";
+
+function AuthButton() {
+    const { data: session } = useSession();
+
+    if (session) {
+        return (
+            <>
+                {session?.user?.name} <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        );
+    }
+    return (
+        <>
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
+        </>
+    )
+}
 
 export default function AdminSidebar() {
     return (
@@ -45,6 +66,7 @@ export default function AdminSidebar() {
                         </Link>
                     </li>
                 </ul>
+                <AuthButton />
             </div>
             </aside>
 
